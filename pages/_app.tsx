ec6e894e37ab634e32e45b3f8eb31466a044792e
rubyandaway <rubyandaway@gmail.com>
2021-12-29 11:48:13 +0000

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,44 @@
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
+import "reset-css";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5%',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDDBDB',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121'
+    },
+  },
+  Component: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadown: 'none'
+          },
+        },
+
+      },
+    },
+  },
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+
+  );
+};
+
+export default MyApp
+
+
