88381990a5e55ea97fae788caeb1cd239f9eb15b
rubyandaway <rubyandaway@gmail.com>
2022-01-01 12:15:56 +0000

    added sidebar and logo

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,19 +1,20 @@
-import { Box } from '@chakra-ui/layout';
+import { Box } from '@chakra-ui/layout'
+import Sidebar from './sidebar'
 
 const PlayerLayout = ({children}) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        sidebar
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
       </Box>
-      <Box position="absolute" left="0" Bottom="0">
+      <Box position="absolute" left="0" bottom="0">
         Player
       </Box>
     </Box>
   );
 };
 
-export default PlayerLayout;
+export default PlayerLayout
